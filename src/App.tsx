454c7571547ca7aa1c1b813b46ee066757acb5cf
SAPSDFO <gui47e@gmail.com>
2025-06-11 17:04:20 -0300

    Replace all carousel images with new ones

--- src/App.tsx
+++ src/App.tsx
@@ -52,16 +52,16 @@ function App() {
   };
 
   const carouselImages = [
+    '/1 (3).jpg',
+    '/1 (5).jpg',
+    '/1 (6).jpg',
     '/1 (7).jpg',
     '/1 (9).jpg',
     '/1 (10).jpg',
     '/1 (11).jpg',
     '/1 (16).jpg',
-    '/2.png',
-    '/3.png',
-    '/4.png',
-    '/5.png',
-    '/Design sem nome.png',
+    '/1 (17).jpg',
+    '/1 (18).jpg',
   ];
 
   if (showPreloader) {
